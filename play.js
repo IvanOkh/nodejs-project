8b4b61813096e541c7958e6393388ccfd2c7812e
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-06-14 19:12:01 -0600

    Added splice, spread, rest operators

--- play.js
+++ play.js
@@ -7,9 +7,31 @@ const person = {
 };
 
 // person.greet();
-const hobbies = ["sports", "cooking"];
-// for (let hobby of hobbies) {
-//   console.log(hobby);
-// }
+const hobbies = ["Sports", "Cooking"];
+for (let hobby of hobbies) {
+  console.log(hobby);
+}
+
+//map touches every element in the array and modifies them
 console.log(hobbies.map((hobby) => "Hobby: " + hobby));
 console.log(hobbies);
+
+hobbies.push("Programming");
+console.log(hobbies);
+
+//slice creates a copy
+const copiedArray = hobbies.slice();
+console.log(copiedArray);
+
+//spread operator spreads data from within other array into the new one
+const copiedArray2 = [...hobbies];
+console.log(copiedArray2);
+//same applies for the objects
+const copiedPerson = { ...person };
+console.log(copiedPerson);
+
+//rest operator
+const toArray = (...args) => {
+  return args;
+};
+console.log(toArray(1, 2, 3, 4));
