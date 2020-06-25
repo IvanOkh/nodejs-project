73f9e1f1ad3a229a984d434afb196ac927ac7ff1
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-06-24 22:44:56 -0600

    Test

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 const http = require("http");
+const fs = require("fs");
 
 //we can use event driven apporach
 const server = http.createServer((req, res) => {
-  console.log(req);
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write(
+      "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push();
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    });
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<p>hello</p>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
