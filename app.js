c448163584b3640fdfa3720416cc453031e58cee
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-06-25 23:21:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   console.log(req);
 });
-
+//startup on port 3000
+console.log("Up and running on localhost:3000");
 server.listen(3000);
