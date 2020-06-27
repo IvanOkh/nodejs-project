0550e95a2b210ebdaf742060548f98689d88db8b
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-06-26 23:50:58 -0600

    Update

--- first-app.js
+++ first-app.js
@@ -1,5 +1,5 @@
 console.log("Hello from Node.js");
-
+//instance of a file system 
 const fs = require("fs"); //fs = file system
-
+//write to a file
 fs.writeFileSync("hello.txt", "Hello from node.js");
