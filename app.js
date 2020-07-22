17d0da3ccc1f06428f9e65e450df2298b9ca0438
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-22 17:40:39 -0600

    Express

--- app.js
+++ app.js
@@ -1,40 +1,47 @@
 const http = require("http");
-const fs = require("fs");
 
-//we can use event driven apporach
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write(
-      "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
-    );
-    res.write("</html>");
-    return res.end();
-  }
+const express = require("express");
 
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push();
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-    });
-    fs.writeFileSync("message.txt", "DUMMY");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    return res.end();
-  }
+const app = express();
 
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<p>hello</p>");
-  res.write("</html>");
-  res.end();
-});
+app.use();
+
+const server = http.createServer(app);
 
 server.listen(3000);
+
+//we can use event driven apporach
+// const server = http.createServer((req, res) => {
+//   const url = req.url;
+//   const method = req.method;
+//   if (url === "/") {
+//     res.write("<html>");
+//     res.write(
+//       "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
+//     );
+//     res.write("</html>");
+//     return res.end();
+//   }
+
+//   if (url === "/message" && method === "POST") {
+//     const body = [];
+//     req.on("data", (chunk) => {
+//       console.log(chunk);
+//       body.push();
+//     });
+//     req.on("end", () => {
+//       const parsedBody = Buffer.concat(body).toString();
+//       console.log(parsedBody);
+//     });
+//     fs.writeFileSync("message.txt", "DUMMY");
+//     res.statusCode = 302;
+//     res.setHeader("Location", "/");
+//     return res.end();
+//   }
+
+//   res.setHeader("Content-Type", "text/html");
+//   res.write("<html>");
+//   res.write("<p>hello</p>");
+//   res.write("</html>");
+//   res.end();
+// });
