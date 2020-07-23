4e5d339df3848a39fe781f4dfd96ed551a2d152c
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-22 18:11:36 -0600

    Switched to Express

--- app.js
+++ app.js
@@ -1,47 +1,10 @@
-const http = require("http");
-
 const express = require("express");
 
 const app = express();
 
-app.use();
+app.use((req, res, next) => {
+  console.log("In another middleware!");
+  res.send("<h1>Hello from Express!</h1>");
+});
 
-const server = http.createServer(app);
-
-server.listen(3000);
-
-//we can use event driven apporach
-// const server = http.createServer((req, res) => {
-//   const url = req.url;
-//   const method = req.method;
-//   if (url === "/") {
-//     res.write("<html>");
-//     res.write(
-//       "<form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form>"
-//     );
-//     res.write("</html>");
-//     return res.end();
-//   }
-
-//   if (url === "/message" && method === "POST") {
-//     const body = [];
-//     req.on("data", (chunk) => {
-//       console.log(chunk);
-//       body.push();
-//     });
-//     req.on("end", () => {
-//       const parsedBody = Buffer.concat(body).toString();
-//       console.log(parsedBody);
-//     });
-//     fs.writeFileSync("message.txt", "DUMMY");
-//     res.statusCode = 302;
-//     res.setHeader("Location", "/");
-//     return res.end();
-//   }
-
-//   res.setHeader("Content-Type", "text/html");
-//   res.write("<html>");
-//   res.write("<p>hello</p>");
-//   res.write("</html>");
-//   res.end();
-// });
+app.listen(3000);
--- first-app.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello from Node.js");
-
-const fs = require("fs"); //fs = file system
-
-fs.writeFileSync("hello.txt", "Hello from node.js");
