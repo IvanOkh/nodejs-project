7a45907f169ae3c2c53e98cca77d378a701de9a6
Ivan Okhrimovich <okhrimovichivan@gmail.com>
2020-07-22 18:32:15 -0600

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-//we can use event driven apporach
-const server = http.createServer((req, res) => {
-  console.log(req);
-});
-//startup on port 3000
-console.log("Up and running on localhost:3000");
-server.listen(3000);
